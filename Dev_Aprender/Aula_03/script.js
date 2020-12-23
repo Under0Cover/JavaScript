2a1e427a45f8c61f91fa0afb4f1c025cee28deef
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-12-23 19:28:10 -0300

    Aula_03 - Tipos Primitos (01)

---
+++ Dev_Aprender/Aula_03/script.js
@@ -0,0 +1,12 @@
+// TIPOS PRIMITIVOS
+
+// TIPOS DE REFERÊNCIAS
+// STRING - NUMBER - BOOLEAN - UNDEFINED - NULL
+let nome = 'Oliver' //STRING LITERAL
+let idade = 25 // NUMBER LITERAL
+let estaAprovado = true // BOOLEAN
+let sobrenome //UNDEFINED
+let corSelecionado = null // NULL 
+
+
+// TIPOS DE VALORES
