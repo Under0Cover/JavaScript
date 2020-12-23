a6c9bde812495f3790016548b0a738a739657f3b
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-12-23 19:38:01 -0300

    Aula_04 - Tipos Primitivos (02)

---
+++ Dev_Aprender/Aula_04/script.js
@@ -0,0 +1,31 @@
+// TIPOS PRIMITIVOS
+
+// TIPOS DE REFERÊNCIAS
+// STRING - NUMBER - BOOLEAN - UNDEFINED - NULL
+let nome = 'Oliver' //STRING LITERAL
+let idade = 25 // NUMBER LITERAL
+let estaAprovado = true // BOOLEAN
+let sobrenome //UNDEFINED
+let corSelecionado = null // NULL 
+
+
+// TIPOS DE VALORES
+console.log(typeof nome)
+nome = 55
+console.log(typeof nome)
+
+console.log(typeof idade)
+idade = 25.05
+console.log(typeof idade)
+
+console.log(typeof estaAprovado)
+estaAprovado = 10
+console.log(typeof estaAprovado)
+
+console.log(typeof sobrenome)
+sobrenome = "Oliver"
+console.log(typeof sobrenome)
+
+console.log(typeof corSelecionado)
+corSelecionado = 'Azul'
+console.log(typeof corSelecionado)
