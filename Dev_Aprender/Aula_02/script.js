32c6aafb65d96563e2f0fa230a96303f80b8d171
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-12-23 19:13:44 -0300

    Aula_02 - Constantes

---
+++ Dev_Aprender/Aula_02/script.js
@@ -0,0 +1,6 @@
+let valorIngressoAdulto = 20
+const valorIngressoInfantil = 10
+valorIngressoAdulto = 30
+// valorIngressoInfantil = 15
+console.log(valorIngressoAdulto)
+console.log(valorIngressoInfantil)
