aa385eeeef417d9f61b48bba7acd42d6f1940846
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-12-24 18:47:55 -0300

    Aula_06 - ARRAY

---
+++ Dev_Aprender/Aula_06/script.js
@@ -0,0 +1,36 @@
+// ARRAYS
+
+let familia = [
+    mae = [
+        nome = 'Joana',
+        sobrenome = 'Batista',
+        idade = 67,
+        estadoCivil = 'casada'
+    ],
+
+    pai = [
+        nome = 'Manoel',
+        sobrenome = 'Batista',
+        idade = 70,
+        estadoCivil = 'casado'
+    ],
+
+    primeiroFilho = [
+        nome = 'Carlos',
+        sobrenome = 'Batista',
+        idade = 31,
+        estadoCivil = 'solteiro'
+    ],
+
+    segundoFilho = [
+        nome = 'Manoel',
+        sobrenome = 'Batista Jr',
+        idade = 28,
+        estadoCivil = 'solteiro'
+    ]
+]
+
+console.log(familia)
+console.log(familia.length)
+console.log(familia[0])
+console.log(familia[1][2])
