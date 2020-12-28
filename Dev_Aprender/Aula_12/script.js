942f00742e4904bf654ea6fed81e5f58a8cc90ff
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-12-28 20:20:20 -0300

    Aula_12 - Operadores (04)

---
+++ Dev_Aprender/Aula_12/script.js
@@ -0,0 +1,52 @@
+// OPERADORES
+
+// OPERADORES ARITIMÉTICOS
+let salario = 100
+
+console.log(salario)
+console.log(salario + salario)
+console.log(salario - salario)
+console.log(salario * salario)
+console.log(salario / salario)
+console.log(salario ** salario)
+
+// INCREMENTO E DECREMENTO
+let idade = 18
+console.log(idade)
+console.log(idade++)
+console.log(idade)
+console.log(++idade)
+console.log(idade)
+console.log(idade--)
+console.log(idade)
+console.log(--idade)
+console.log(idade)
+
+// OPERADORES DE ATRIBUIÇÃO
+let valorTecladoGamer = 100
+console.log(valorTecladoGamer)
+valorTecladoGamer += valorTecladoGamer
+console.log(valorTecladoGamer)
+valorTecladoGamer -= valorTecladoGamer
+console.log(valorTecladoGamer)
+
+// OPERADORES DE COMPARAÇÃO
+// IGUALDADE ESTRITA
+console.log(1 === 1)
+console.log('1' === 1)
+console.log('1' === '1')
+// IGUALDADE SOLTA
+console.log(1 == 1)
+console.log('1' == 1)
+console.log('1' == '1')
+
+
+// OPERADOR TERNÁRIO
+// O CLIENTE QUE JUNTAR MAIS DE 100 PONTOS É CLIENTE PREMIUM
+let pontos = 101
+let tipoCliente = pontos > 100 ? 'Premium' : 'Normal'
+console.log(tipoCliente)
+
+// OPERADORES LÓGICOS
+
+// OPERADORES BITWISE
