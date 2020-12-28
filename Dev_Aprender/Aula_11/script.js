66c5d52ee22cd3258fd9fa0669913d171301b25b
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-12-28 20:05:45 -0300

    Aula_11 - Operadores (03)

---
+++ Dev_Aprender/Aula_11/script.js
@@ -0,0 +1,46 @@
+// OPERADORES
+
+// OPERADORES ARITIMÉTICOS
+let salario = 100
+
+console.log(salario)
+console.log(salario + salario)
+console.log(salario - salario)
+console.log(salario * salario)
+console.log(salario / salario)
+console.log(salario ** salario)
+
+// INCREMENTO E DECREMENTO
+let idade = 18
+console.log(idade)
+console.log(idade++)
+console.log(idade)
+console.log(++idade)
+console.log(idade)
+console.log(idade--)
+console.log(idade)
+console.log(--idade)
+console.log(idade)
+
+// OPERADORES DE ATRIBUIÇÃO
+let valorTecladoGamer = 100
+console.log(valorTecladoGamer)
+valorTecladoGamer += valorTecladoGamer
+console.log(valorTecladoGamer)
+valorTecladoGamer -= valorTecladoGamer
+console.log(valorTecladoGamer)
+
+// OPERADORES DE COMPARAÇÃO
+// IGUALDADE ESTRITA
+console.log(1 === 1)
+console.log('1' === 1)
+console.log('1' === '1')
+// IGUALDADE SOLTA
+console.log(1 == 1)
+console.log('1' == 1)
+console.log('1' == '1')
+
+
+// OPERADORES LÓGICOS
+
+// OPERADORES BITWISE
