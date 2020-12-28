106d3ced25de94b93ca28fd789781c867ab6b784
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-12-28 19:44:06 -0300

    Aula_10 - Operadores (02)

---
+++ Dev_Aprender/Aula_10/script.js
@@ -0,0 +1,37 @@
+// OPERADORES
+
+// OPERADORES ARITIMÉTICOS
+let salario = 100
+
+console.log(salario)
+console.log(salario + salario)
+console.log(salario - salario)
+console.log(salario * salario)
+console.log(salario / salario)
+console.log(salario ** salario)
+
+// INCREMENTO E DECREMENTO
+let idade = 18
+console.log(idade)
+console.log(idade++)
+console.log(idade)
+console.log(++idade)
+console.log(idade)
+console.log(idade--)
+console.log(idade)
+console.log(--idade)
+console.log(idade)
+
+// OPERADORES DE ATRIBUIÇÃO
+let valorTecladoGamer = 100
+console.log(valorTecladoGamer)
+valorTecladoGamer += valorTecladoGamer
+console.log(valorTecladoGamer)
+valorTecladoGamer -= valorTecladoGamer
+console.log(valorTecladoGamer)
+
+// OPERADORES DE COMPARAÇÃO
+
+// OPERADORES LÓGICOS
+
+// OPERADORES BITWISE
