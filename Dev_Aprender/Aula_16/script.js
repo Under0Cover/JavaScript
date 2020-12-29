37e43a5bde6618eb3c2aa8feba215bfbe3b18da4
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-12-29 14:24:00 -0300

    Aula_16 - Exercício 01

---
+++ Dev_Aprender/Aula_16/script.js
@@ -0,0 +1,17 @@
+// EXERCÍCIO 01
+// TROCAR AS VARIÁVEIS DE VALOR
+
+let a = 'vermelho'
+let b = 'azul'
+
+console.log(a)
+console.log(b)
+
+// MINHA FORMA DE TROCAR
+let c
+c = a
+a = b
+b = c
+
+console.log(a)
+console.log(b)
