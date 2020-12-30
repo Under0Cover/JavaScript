6eb3ec491f150439aa0796f57eecdcf0efbc4ecf
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-12-30 11:43:45 -0300

    Aula_21 - LAÇO DE REPETIÇÃO (DO-WHILE)

---
+++ Dev_Aprender/Aula_21/script.js
@@ -0,0 +1,58 @@
+// LOOPS - LAÇOS DE REPETIÇÃO
+
+// FOR
+// FOR SIMPLES
+for (let indice = 0; indice < 5; indice++) {
+    console.log('Estou aprendendo', indice + 1)
+}
+// FOR COM IF (PARA IMPRIMI APENAS NÚMEROS IMPARES)
+for (let indice = 0; indice <= 5; indice++) {
+    if (indice % 2 !== 0) {
+        console.log('Apenas índices impares ', indice)
+    }
+}
+// FOR COM IF COM DECREMENTO (PARA IMPRIMIR APENAS NÚMEROS PARES)
+for (let indice = 5; indice >= 0; indice--) {
+    if (indice % 2 == 0) {
+        console.log('Apenas índices pares, com decremento ', indice)
+    }
+}
+
+// WHILE
+// REUTILIZAÇÃO DA VARIÁVEL INDICE DURANTE O PROCESSO DOS DIFERNETES WHILE
+let indice = 5
+
+// WHILE SIMPLES COM DECREMENTO
+while (indice >= 1) {
+    console.log('Usando o WHILE ',indice)
+    indice--
+}
+// INDICE = 0
+
+// WHILE COM IF (PARA IMPRIMIR APENAS NÚMEROS IMPARES)
+while (indice <= 5){
+    if (indice % 2 !== 0){
+        console.log('Apenas índices impares ',indice)
+    }
+    indice++
+}
+// INDICE = 6
+
+// WHILE COM IF (PARA IMPRIMIR NÚMEROS IMPARES) COM DECREMENTO
+while (indice >= 1) {
+    if (indice % 2 == 0) {
+        console.log('Apenas índices pares ',indice)
+    }
+    indice--
+}
+// INDICE = 0
+
+// DO - WHILE
+do {
+    console.log('digitando... ', indice + 1)
+    indice++
+} while (indice < 10)
+
+// FOR - IN
+
+// FOR - OF
