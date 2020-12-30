dc06efd2ad090f21f3a540284b7457939fb43489
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2020-12-30 09:35:04 -0300

    Aula_19 - LAÇO DE REPETIÇÃO (FOR)

---
+++ Dev_Aprender/Aula_19/script.js
@@ -0,0 +1,27 @@
+// LOOPS - LAÇOS DE REPETIÇÃO
+
+// FOR
+// FOR SIMPLES
+for (let indice = 0; indice < 5; indice++) {
+    console.log('Estou aprendendo', indice + 1)
+}
+// FOR COM IF (PARA IMPRIMI APENAS NÚMEROS IMPARES)
+for (let indice = 0; indice <= 5; indice++) {
+    if (indice % 2 !== 0) {
+        console.log('Apenas índices impares ', indice)
+    }
+}
+// FOR COM IF COM DECREMENTO (PARA IMPRIMIR APENAS NÚMEROS PARES)
+for (let indice = 5; indice >= 0; indice--) {
+    if (indice % 2 == 0) {
+        console.log('Apenas índices pares, com decremento ', indice)
+    }
+}
+
+// WHILE
+
+// DO - WHILE
+
+// FOR - IN
+
+// FOR - OF
