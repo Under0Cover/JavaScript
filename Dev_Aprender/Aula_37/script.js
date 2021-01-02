930dcc67434542b377701c93523c199e16ab8c82
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2021-01-02 09:40:35 -0300

    Aula_37 - Biblioteca Math

---
+++ Dev_Aprender/Aula_37/script.js
@@ -0,0 +1,50 @@
+// Biblioteca Math
+
+// Apresentado na Aula
+// Randomiza um número aleatório entre 0 e 1
+console.log(Math.random())
+
+// Seleciona o valor máximo
+console.log(Math.max(3,6,8,10,15))
+
+// Seleciona o valor mínimo
+console.log(Math.min(3,6,8,10,15))
+
+// Apresentado no developer.mozilla.org
+
+// Propriedades
+// Constante de Euler
+console.log(Math.E)
+
+// Logaritmo natural de 2
+console.log(Math.LN2)
+
+// Logaritmo natural de 10
+console.log(Math.LN10)
+
+// Logaritmo de Euler na base 2
+console.log(Math.LOG2E)
+
+// Logaritmo de Euler na base 10
+console.log(Math.LOG10E)
+
+// Número de PI
+console.log(Math.PI)
+
+// Raiz Quadrada de 1/2
+console.log(Math.SQRT1_2)
+
+// Raiz Quadrada de 2
+console.log(Math.SQRT2)
+
+// Métodos
+// Retorna o módulo
+console.log(Math.abs(-34))
+
+// Retorna o arco-coseno
+console.log(Math.acos(0.7))
+
+// Retorna o arco-coseno hiperbólico
+console.log(Math.acosh(30))
+
+// Entre outras....
