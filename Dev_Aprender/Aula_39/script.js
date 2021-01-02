6275b9da02f560f47275a78d6e88e6831ad9129b
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2021-01-02 10:42:22 -0300

    Aula_39 - Template literal

---
+++ Dev_Aprender/Aula_39/script.js
@@ -0,0 +1,21 @@
+// Template literal
+
+const mensagem = 'Oi, essa é a minha primeira mensagem!'
+console.log(mensagem)
+
+const outra = `Oi, essa é a minha
+'primeira' mensagem!`
+console.log(outra)
+
+const nome = 'Oliver'
+const email = 
+`Olá, ${nome}
+
+Obrigado por se inscrever no canal!
+
+Para acompanhar os vídeos não se esqueça de ativar o sininho!
+Você ganhou  R$ ${10 + 5} em vale compras!
+
+Obrigado,
+Fulano da Silva`
+console.log(email)
