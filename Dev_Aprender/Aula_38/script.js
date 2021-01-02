116f07ab795b7f06a2ff6c4d3b15779fb7cda5d3
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2021-01-02 10:27:44 -0300

    Aula_38 - Strings

---
+++ Dev_Aprender/Aula_38/script.js
@@ -0,0 +1,23 @@
+// String
+
+// Tipo Primitivo
+const mensagem = 'Minha primeira mensagem!'
+console.log(mensagem)
+console.log(mensagem.includes('primeira'))
+console.log(mensagem.includes('vermelho'))
+console.log(mensagem.startsWith('Minha'))
+console.log(mensagem.startsWith('Verde'))
+console.log(mensagem.endsWith('mensagem!'))
+console.log(mensagem.endsWith('mensagem.'))
+console.log(mensagem.indexOf('primeira'))
+console.log(mensagem.replace('Minha', 'Sua'))
+console.log(mensagem.trim())
+console.log(mensagem.split(' '))
+
+
+// Tipo Objeto
+const outraMensagem = new String('Bom dia!')
+console.log(outraMensagem)
+console.log(outraMensagem.length)
+console.log(outraMensagem[2])
+
