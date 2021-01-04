2c0a0b24ab7dd9fc9a91b69e0985f3188abb6cc4
Under0Cover <60072961+Under0Cover@users.noreply.github.com>
2021-01-04 10:40:41 -0300

    Aula_46 - Array (01)

---
+++ Dev_Aprender/Aula_46/script.js
@@ -0,0 +1,23 @@
+// Introdução a Arrays
+
+// Adicionar novos elementos
+const numeros = [1,2,3]
+
+// Inserindo elementos:
+// - no início
+numeros.unshift(0)
+console.log(numeros)
+
+// - no meio
+numeros.splice(1,0,25)
+console.log(numeros)
+
+// - no fim
+numeros.push(5)
+console.log(numeros)
+
+// Encontrar elementos
+// Remover elementos
+// Dividir elementos
+// Dividir arrays
+// Combinar arrays
